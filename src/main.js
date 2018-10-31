01dc63fcb65b7095090f898b5e9d8320a29275c6
litao <ltgdf888@163.com>
2018-10-31 16:03:40 +0800

    将axios做成Vue插件

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// 导入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 导入自己做的axios（Vue插件）
+import HttpTool from './publign/http'
 import router from './router'
+
+Vue.use(ElementUI)
+Vue.use(HttpTool)
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: {
+        App
+    },
+    template: '<App/>'
 })
